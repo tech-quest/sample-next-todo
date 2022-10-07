ca37e0e685160053815036370d257d64fb6a9abf
kuro-xx <k.kuroda@miwaza.co.jp>
2022-10-07 11:48:30 +0900

    feat: add task input state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,25 @@
+import React, { useState } from 'react';
 import type { NextPage } from 'next';
 import styles from '~/styles/Home.module.css';
 
 const Home: NextPage = () => {
+  // MEMO: 入力したタスクの値を保存するState
+  const [task, setTask] = useState<string>('');
+
+  // MEMO: タスク入力欄が変更された場合の処理
+  const handleTaskChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // MEMO: タスクの値をStateに保存する
+    setTask(e.target.value);
+  };
+
+  // フォームを送信した場合(追加ボタンをクリックした場合)の処理
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // MEMO: formタグのデフォルトの挙動 (送信処理) を無効化
+    e.preventDefault();
+    // MEMO: タスクの値をアラート表示して確認
+    alert(task);
+  };
+
   return (
     <>
       <div className={styles.panel}>
@@ -10,8 +28,15 @@ const Home: NextPage = () => {
         </div>
         <div className="panelBody">
           <div className={styles.inputField}>
-            <form noValidate className={styles.form}>
-              <input className={styles.input} placeholder="タスクを入力してください" type="text" name="task" />
+            <form noValidate className={styles.form} onSubmit={handleSubmit}>
+              <input
+                className={styles.input}
+                placeholder="タスクを入力してください"
+                type="text"
+                name="task"
+                value={task}
+                onChange={handleTaskChange}
+              />
               <button type="submit" className={styles.formButton}>
                 追加
               </button>
